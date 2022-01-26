d12f2e5a87664983c2df088f3b89910199f48c16
CJ Ferguson <ramat.fergus@gmail.com>
2022-01-25 16:54:14 -0800

    Added maximum limit on grid sizes. Code finished and ready for submission. Will work on better git syle next go'round :)

--- designs.js
+++ designs.js
@@ -1,50 +1,33 @@
-// Select color input
-// Select size input
-
-const maxWidth = 300;
-const maxHeight = maxWidth;
 const submitGrid = document.getElementById('sizePicker');
 const colorChoice = document.getElementById('colorPicker');
 const theCanvas = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
 submitGrid.addEventListener('submit', function(event) {
                             makeGrid();
-                            event.preventDefault(); }
-                           ); // Need this to get form not to reset?
+                            event.preventDefault();
+                            }); // Need this to get form not to reset?
 
-function cellColorChange(item) {
-  item.style.bgColor = colorChoice.value;
-  alert("clicked on box, " + item);
-  console.log(Object.keys(item));
+// ====================================================
+function cellColorChange(myCell) {
+  let selectColor = "background-color:" + colorChoice.value;
+  myCell.setAttribute('style',selectColor);
 }
 
+// ====================================================
 function makeGrid() {
-
-  let msg = "height: " + this.inputHeight.value +
-    "\nwidth: " + this.inputWidth.value;
-
-  for(let row = 1; row <= 30 /*this.inputHeight.value*/; row++) {
-    const newRowElement = document.createElement('tr');
-    for(let col = 0; col < 30 /*this.inputHeight.value*/; col++) {
-        newRowElement.insertCell(col);
-        theCanvas.appendChild(newRowElement);
-    }  // col loop
+  theCanvas.textContent = ""; // reset canvas
+  for (let row = 1; row <= this.inputHeight.value; row++) {
+      const newRowElement = document.createElement('tr');
+      for (let col = 0; col < this.inputWidth.value; col++) {
+          newRowElement.insertCell(col);
+          theCanvas.appendChild(newRowElement);
+      }  // col loop
   }  // row loop
 
-  const td = theCanvas.querySelectorAll('td');
-  //alert("td count= "+td.length);
-  //td.style.bgColor = colorChoice.value;
-  //alert("td length= " + td.length);
-  let c =0;
-  td.forEach(function(tdItem) {
-    if(c===1) { console.log("tdItem: " + Object.keys(tdItem));}
-    tdItem.addEventListener('click',function() {
-      //tdItem.style.bgColor = 'green'; //colorChoice.value;
-      cellColorChange(tdItem);
-      c++;
-    });
-  });
-
-
+  const td = theCanvas.querySelectorAll('td'); // load cells with color
+    td.forEach(function(item) {                // change function
+        item.addEventListener('click',function() {
+          cellColorChange(item);
+        });
+      });
 }
